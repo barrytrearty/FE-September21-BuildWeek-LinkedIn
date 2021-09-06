b4ce90bb971486564fc7ce9336bc89baffc908d1
RoosaJuliaV <86317676+RoosaJuliaV@users.noreply.github.com>
2021-09-06 17:45:22 +0300

    Navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import Profile from './pages/Profile';
+import Navbar from "./components/Navbar"
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
 
 function App() {
-  return <div>linkedin</div>;
+  return (
+    <Router>
+      <Route exact path='/' component={Profile} />
+      <Route path='/' component={Navbar} />
+      <Route path='/' component={Footer} />
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
