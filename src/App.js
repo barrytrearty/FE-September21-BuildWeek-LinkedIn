71a689e39226336c426deb6db7d6049b6364e4ff
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-06 15:01:47 +0100

    aftermain component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import AfterMain from "./components/AfterMain";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    // <div className="App">
+    <div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <AfterMain />
     </div>
   );
 }
