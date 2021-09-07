878fe7b3c758295c011f2b93033e21911f0eeee6
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-07 14:40:03 +0100

    after main cards, date formatted

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Row, Col, Container, Card, Button } from "react-bootstrap";
 import StriveLogo from "../data/logo.png";
 import EditModal from "./EditModal";
+import { format, parseISO } from "date-fns";
 
 const Experiences = ({ match }) => {
   // const experienceId = match.params.id;
@@ -12,9 +13,12 @@ const Experiences = ({ match }) => {
     try {
       let response = await fetch(
         `https://striveschool-api.herokuapp.com/api/profile/${experienceId}/experiences`,
-        {headers: {
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZTBiMTJkNTI2MjAwMTViNmRjOGQiLCJpYXQiOjE2MzEwMTE2NDMsImV4cCI6MTYzMjIyMTI0M30.wxo-L7vPKDv0DeIAf5S_h2cwEHJqqvPcL0Il7sQlPYs"
-          }}
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZTBiMTJkNTI2MjAwMTViNmRjOGQiLCJpYXQiOjE2MzEwMTE2NDMsImV4cCI6MTYzMjIyMTI0M30.wxo-L7vPKDv0DeIAf5S_h2cwEHJqqvPcL0Il7sQlPYs",
+          },
+        }
       );
       let experienceArray1 = await response.json();
       setExperienceArray(experienceArray1);
@@ -23,23 +27,30 @@ const Experiences = ({ match }) => {
     }
   };
 
-
-
+  const fixDate = (date) => {
+    try {
+      return format(
+        parseISO(date), // 1)
+        "yyyy MMMM dd"
+      );
+    } catch {
+      return `No date found`;
+    }
+  };
 
   useEffect(() => {
     getArray();
   });
 
-
   return (
-    <section className="px-5 py-2">
+    <Card className="px-4 py-2">
       <Row>
-        <h3>Experience</h3>
+        <Card.Title className="px-3">Experience</Card.Title>
       </Row>
       {console.log(experienceArray)}
 
       {experienceArray.map((experience) => (
-        <Row className="border-bottom" key={experience._id}>
+        <Row key={experience._id}>
           <Col xs={2}>
             <img src={StriveLogo} alt="" className="mt-3" />
           </Col>
@@ -47,13 +58,13 @@ const Experiences = ({ match }) => {
             <p className="p-heading">{experience.role}</p>
             <p className="p-secondary">{experience.company}</p>
             <p className="p-muted">
-              {experience.startDate} - {experience.EndDate}{" "}
+              {fixDate(experience.startDate)} - {fixDate(experience.endDate)}{" "}
             </p>
             <p className="p-secondary">{experience.description}</p>
           </Col>{" "}
         </Row>
       ))}
-    </section>
+    </Card>
   );
 };
 
