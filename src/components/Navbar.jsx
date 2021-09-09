1df668eec89a6f1617ea6d5b8814c48b47b21b50
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-09-09 20:01:33 +0200

    fixed typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,13 +85,7 @@ class BootstrapNavbar extends React.Component {
                             className="ml-2 navicon"
 
                             color="#4d4d4d"
-                          /> */}
-                          <svg className="ml-3" height="25" width="25" viewBox="0 0 30 30">
-                <path
-                  transform="translate(2, 2)"
-                  d="M10 10h4v4h-4v-4zm0 11h4v-4h-4v4zm-7-7h4v-4H3v4zm0 7h4v-4H3v4zM3 7h4V3H3v4zm14 7h4v-4h-4v4zm0-11v4h4V3h-4zm-7 4h4V3h-4v4zm7 14h4v-4h-4v4z"
-                ></path>
-              </svg>
+                          /> 
                           <br />
                           Home
                         </Nav.Link>
