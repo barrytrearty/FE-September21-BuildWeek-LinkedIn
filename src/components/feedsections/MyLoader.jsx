050648176d50fff2ec7d3efb156860aefde633a8
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-10 11:47:42 +0100

    Grey

--- src/components/feedsections/MyLoader.jsx
+++ src/components/feedsections/MyLoader.jsx
@@ -4,17 +4,17 @@ import ContentLoader from "react-content-loader";
 const MyLoader = (props) => (
   <ContentLoader
     speed={3}
-    width={450}
+    width={500}
     height={160}
-    viewBox="0 0 450 160"
+    viewBox="0 0 500 160"
     backgroundColor="#f3f3f3"
-    foregroundColor="#0072B1"
+    foregroundColor="#56626862"
     {...props}
   >
     <rect x="0" y="9" rx="3" ry="3" width="109" height="16" />
     <rect x="0" y="43" rx="3" ry="3" width="109" height="16" />
 
-    <rect x="300" y="28" rx="0" ry="0" width="124" height="34" />
+    <rect x="400" y="28" rx="0" ry="0" width="124" height="34" />
     <rect x="0" y="73" rx="3" ry="3" width="100%" height="5" />
     <rect x="0" y="83" rx="3" ry="3" width="100%" height="5" />
     <rect x="0" y="93" rx="3" ry="3" width="100%" height="5" />
